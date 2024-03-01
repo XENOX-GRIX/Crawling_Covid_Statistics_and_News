68ffc4143fb19223a5524d1a5ad23d30acbde286
abhinandan12345678 <abhinandan.mondal2001@gmail.com>
2024-03-02 02:33:29 +0530

    responses pages are crawled

--- Wikipedia/main.py
+++ Wikipedia/main.py
@@ -2,8 +2,10 @@ from helper import webpage_download
 from helper import webpages
 from helper import timeline
 from helper import project
+from helper import responses
 
 webpage_download.web_dwnload()
 project.wiki_crawl()
 webpages.categorize_and_download()
 timeline.crawls()
+responses.crawls()
