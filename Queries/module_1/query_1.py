275fa3cb7a7235b2ff2093dbd3e304a40831db13
XENOX-GRIX <xenoxgrik@gmail.com>
2024-03-02 15:21:44 +0530

    Module 3.1 Completed

--- Queries/module_1/query_1.py
+++ Queries/module_1/query_1.py
@@ -7,14 +7,7 @@ folder_path = "../../worldometer/Results/data"
 # Get all file names in the folder
 file_names = ["../../worldometer/Results/data/"+f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
-# # List of day files
-# day_files = ["../../worldometer/Results/table_data.txt"] 
 input_mappings = [2,8,4,6,12,11,13,3,5,7]
-# # Run the commands for each day and pipe the output
-
-# command = f"python mapper.py {day_files[0]} 2 | python combiner.py  | python reducer.py 3 > network.txt"
-# # Execute the entire command
-# subprocess.run(command, shell=True)
 country_mappings = {} 
 f = open("../../worldometer/Results/table_data.txt")
 for line in f : 
@@ -48,17 +41,17 @@ def menu():
                 break
             index = input_mappings[int(choice) - 1]
             # Create the first process
-            first_command = f"python mapper.py {file_names[0]} {index} "
+            first_command = f"python3 mapper.py {file_names[0]} {index} "
             first_process = subprocess.Popen(first_command, shell=True, stdout=subprocess.PIPE)
 
             # Create subsequent processes in a loop, connecting input and output
             for file_name in file_names[1:]:
-                command = f"python combiner.py && python mapper.py {file_name} {index}"
+                command = f"python3 combiner.py && python3 mapper.py {file_name} {index}"
                 process = subprocess.Popen(command, shell=True, stdin=first_process.stdout, stdout=subprocess.PIPE)
                 first_process = process  # Update first_process to connect subsequent processes
 
             # Create the last process with the reducer command
-            last_command = f"python reducer.py {country_mappings[country]}"
+            last_command = f"python3 reducer.py {country_mappings[country]}"
             last_process = subprocess.Popen(last_command, shell=True, stdin=first_process.stdout, stdout=subprocess.PIPE)
 
             # Wait for the last process to finish and get the output
@@ -68,32 +61,3 @@ def menu():
             print("\n" + output.decode())
  
 menu()
-
-# index = 2
-# country = 'india'
-
-# # Create the first process
-# first_command = f"python mapper.py {file_names[0]} {index} "
-# first_process = subprocess.Popen(first_command, shell=True, stdout=subprocess.PIPE)
-
-# # Create subsequent processes in a loop, connecting input and output
-# for file_name in file_names[1:]:
-#     command = f"python combiner.py && python mapper.py {file_name} {index}"
-#     process = subprocess.Popen(command, shell=True, stdin=first_process.stdout, stdout=subprocess.PIPE)
-#     first_process = process  # Update first_process to connect subsequent processes
-
-# # Create the last process with the reducer command
-# last_command = f"python reducer.py {country_mappings[country]} > network.txt"
-# last_process = subprocess.Popen(last_command, shell=True, stdin=first_process.stdout, stdout=subprocess.PIPE)
-
-# # Wait for the last process to finish and get the output
-# output, _ = last_process.communicate()
-
-# # Check for errors or print output
-# if last_process.returncode == 0:
-#     print("Command executed successfully.")
-# else:
-#     print(f"Error executing command. Exit code: {last_process.returncode}")
-
-# # Print the output if needed
-# print("Command output:", output.decode())
